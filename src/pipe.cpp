d97b14b7b4609b5b66f4ce9ee4294080db761b3c
Slobodchikov Roman <romka-sl@vtomske.ru>
2017-08-31 22:15:42 +0700

    Отформатирован

--- src/pipe.cpp
+++ src/pipe.cpp
@@ -10,14 +10,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-int main() {
- int data_processed;
- int file_pipes[2];
- const char some_data[] = "123";
- char buffer[BUFSIZ + 1];
- memset(buffer, 0, sizeof(buffer));
- if (pipe(file_pipes) == 0)
- {
+
+int main()
+{
+	int data_processed;
+	int file_pipes[2];
+	const char some_data[] = "123";
+	char buffer[BUFSIZ + 1];
+	memset(buffer, 0, sizeof(buffer));
+
+	if (pipe(file_pipes) == 0)
+	{
 
 	 pid_t pid = -1;
 
@@ -25,48 +28,39 @@ int main() {
 
 	 switch(pid)
 	 {
-	 case 0:	/* child */
-	 {
-		 printf("CHILD\n");
-		 for(int i = 0; i < 1000000; i++)
+		 case 0:	/* child */
 		 {
-		  data_processed = read(file_pipes[0], buffer, BUFSIZ);
-		  printf("Read %d bytes: %s\n", data_processed, buffer);
+			 printf("CHILD\n");
+			 for(int i = 0; i < 1000000; i++)
+			 {
+			  data_processed = read(file_pipes[0], buffer, BUFSIZ);
+			  printf("Read %d bytes: %s\n", data_processed, buffer);
 
-		  sleep(2);
+			  sleep(2);
+			 }
+			 break;
 		 }
-		 break;
-	 }
-	 case -1:	/* error fork */
-	 {
-		 printf("Fork ERROR\n");
-		 break;
-	 }
-	 default:
-	 {
-		 printf("PARENT\n");
-
-		 for(int i = 0; i < 1000000; i++)
+		 case -1:	/* error fork */
 		 {
-		  data_processed = write(file_pipes[1], some_data, strlen(some_data));
-		  printf("Wrote %d bytes\n", data_processed);
-		  sleep(1);
+			 printf("Fork ERROR\n");
+			 break;
 		 }
-		 break;
+		 default:
+		 {
+			 printf("PARENT\n");
+
+			 for(int i = 0; i < 1000000; i++)
+			 {
+			  data_processed = write(file_pipes[1], some_data, strlen(some_data));
+			  printf("Wrote %d bytes\n", data_processed);
+			  sleep(1);
+			 }
+			 break;
+		 }
 	 }
-	 }
 
+	 exit(EXIT_SUCCESS);
+	}
 
-
-
-
-
-
-
-
-
-
-  exit(EXIT_SUCCESS);
- }
  exit(EXIT_FAILURE);
 }
