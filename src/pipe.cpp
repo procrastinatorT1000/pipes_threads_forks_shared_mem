629a0d35592a40bb8e65b46bd9fc340f8fc94051
Slobodchikov Roman <romka-sl@vtomske.ru>
2017-09-04 21:40:33 +0700

    Процессы A и B вынесены в отдельные функции

--- src/pipe.cpp
+++ src/pipe.cpp
@@ -19,7 +19,7 @@
 #define  SHR_MEM_FILLED     1
 #define  SHR_MEM_TAKEN      -1
 
-#define SHARED_MEMORY_OBJECT_NAME "my_shared_memory"
+#define SHARED_MEMORY_OBJECT_NAME "sqr_value_container"
 #define SHARED_MEMORY_OBJECT_SIZE sizeof(SQR_SHR_MEM_OBJ)
 
 typedef struct
@@ -34,16 +34,114 @@ typedef struct
 	SQR_SHR_MEM_OBJ *pShrMem;
 }THREAD_ARG;
 
-void processA()
+int processC(SQR_SHR_MEM_OBJ *pShrMem);
+
+/*
+ * Process A, reads stdI and writes it to pipe
+ *
+ * writePD - is pipe descriptor for writing
+ *
+ * */
+void processA(int writePD)
 {
+	size_t writenBlen = 0;
+	const char some_data[] = "123";
+
+	 for(int i = 0; i < 20; i++)
+	 {
+		 writenBlen = write(writePD, some_data, strlen(some_data));
+		  printf("Wrote %d bytes\n", writenBlen);
+		  sleep(1);
+	 }
 
 	return;
 }
 
-void processB()
+/*
+ * Process B, reads data from A process with pipe
+ * 			creates process C,
+ * 			squares read data and send it to proc. C
+ * 			with shared memory
+ *
+ * readPD - is pipe descriptor for reading
+ *
+ * */
+int processB(int readPD)
 {
+	 int shm = 0;
+	 SQR_SHR_MEM_OBJ *pShrMemObj = NULL;
 
-	return;
+	 if ( (shm = shm_open(SHARED_MEMORY_OBJECT_NAME, O_CREAT|O_RDWR, S_IRWXO|S_IRWXG|S_IRWXU)) == -1 )
+	 {
+		perror("shm_open");
+		return 1;
+	 }
+
+	 if ( ftruncate(shm, SHARED_MEMORY_OBJECT_SIZE+1) == -1 )
+	 {
+		perror("ftruncate");
+		return 1;
+	 }
+
+	 if ( (pShrMemObj = (SQR_SHR_MEM_OBJ *) mmap(0, SHARED_MEMORY_OBJECT_SIZE+1,
+			 	 PROT_WRITE|PROT_READ, MAP_SHARED, shm, 0)) == (void*)-1 )
+	 {
+		 perror("mmap");
+		 return 1;
+	 }
+
+	 memset(pShrMemObj, 0, SHARED_MEMORY_OBJECT_SIZE);
+
+	 pid_t pid1 = -1;
+
+	 pid1 = fork();
+
+	 switch(pid1)
+	 {
+		 case 0:
+			 /** Process C */
+			 printf("Child 2\n");
+
+			 processC(pShrMemObj);
+
+			 _exit(0);
+			 break;
+		 case -1:
+			 perror("FORK error!\n");
+			 break;
+		 default:
+		 {
+			int data_processed = 0;
+			char buffer[BUFSIZ + 1];
+			memset(buffer, 0, sizeof(buffer));
+
+			 for(int i = 0; i < 100; i++)
+			 {
+				  data_processed = read(readPD, buffer, BUFSIZ);
+				  printf("Read %d bytes: %s\n", data_processed, buffer);
+
+
+
+				  if(pShrMemObj->operationStatus == SHR_MEM_TAKEN ||
+						  pShrMemObj->operationStatus == SHR_MEM_NOT_READY)
+				  {
+					  printf("Val SENT with ShrMEM %d\n", i);
+					  pShrMemObj->sqrVal = i;
+					  pShrMemObj->operationStatus = SHR_MEM_FILLED;
+				  }
+				  sleep(2);
+			 }
+
+			munmap(pShrMemObj, SHARED_MEMORY_OBJECT_SIZE+1);
+			close(shm);
+			shm_unlink(SHARED_MEMORY_OBJECT_NAME);
+
+			break;
+		 }
+	 }
+
+
+	return EXIT_SUCCESS;
 }
 
 void * readValFromSharedMem(void *arg)
@@ -90,20 +188,23 @@ int processC(SQR_SHR_MEM_OBJ *pShrMem)
 
 
 	status = pthread_create(&thread1, NULL, readValFromSharedMem, &thread1Arg);
-	if (status != 0) {
+	if (status != 0)
+	{
 		perror("Создание первого потока!");
 		return EXIT_FAILURE;
 	}
 
 	id2 = 5;
 	status = pthread_create(&thread2, NULL, showThatImAlive, &id2);
-	if (status != 0) {
+	if (status != 0)
+	{
 		perror("Создание второго потока");
 		return EXIT_FAILURE;
 	}
 
 	status = pthread_join(thread1, NULL);
-	if (status != 0) {
+	if (status != 0)
+	{
 		perror("Ждём первый поток");
 		return EXIT_FAILURE;
 	}
@@ -113,7 +214,8 @@ int processC(SQR_SHR_MEM_OBJ *pShrMem)
 	}
 
 	status = pthread_join(thread2, NULL);
-	if (status != 0) {
+	if (status != 0)
+	{
 		perror("Ждём второй поток");
 		return EXIT_FAILURE;
 	}
@@ -127,11 +229,7 @@ int processC(SQR_SHR_MEM_OBJ *pShrMem)
 
 int main()
 {
-	int data_processed;
 	int file_pipes[2];
-	const char some_data[] = "123";
-	char buffer[BUFSIZ + 1];
-	memset(buffer, 0, sizeof(buffer));
 
 	if (pipe(file_pipes) == 0)
 	{
@@ -147,73 +245,7 @@ int main()
 			 /** Process B */
 			 printf("CHILD 1\n");
 
-			 int shm = 0;
-			 SQR_SHR_MEM_OBJ *pShrMemObj = NULL;
-
-			 if ( (shm = shm_open(SHARED_MEMORY_OBJECT_NAME, O_CREAT|O_RDWR, S_IRWXO|S_IRWXG|S_IRWXU)) == -1 )
-			 {
-				perror("shm_open");
-				return 1;
-			 }
-
-			 if ( ftruncate(shm, SHARED_MEMORY_OBJECT_SIZE+1) == -1 )
-			 {
-				perror("ftruncate");
-				return 1;
-			 }
-
-			 if ( (pShrMemObj = (SQR_SHR_MEM_OBJ *) mmap(0, SHARED_MEMORY_OBJECT_SIZE+1,
-					 	 PROT_WRITE|PROT_READ, MAP_SHARED, shm, 0)) == (void*)-1 )
-			 {
-				 perror("mmap");
-				 return 1;
-			 }
-
-			 memset(pShrMemObj, 0, SHARED_MEMORY_OBJECT_SIZE);
-
-			 pid_t pid1 = -1;
-
-			 pid1 = fork();
-
-			 switch(pid1)
-			 {
-				 case 0:
-					 /** Process C */
-					 printf("Child 2\n");
-
-					 processC(pShrMemObj);
-
-					 _exit(0);
-					 break;
-				 case -1:
-					 perror("FORK error!\n");
-					 break;
-				 default:
-					 for(int i = 0; i < 100; i++)
-					 {
-						  data_processed = read(file_pipes[0], buffer, BUFSIZ);
-						  printf("Read %d bytes: %s\n", data_processed, buffer);
-
-
-
-						  if(pShrMemObj->operationStatus == SHR_MEM_TAKEN ||
-								  pShrMemObj->operationStatus == SHR_MEM_NOT_READY)
-						  {
-							  printf("Val SENT with ShrMEM %d\n", i);
-							  pShrMemObj->sqrVal = i;
-							  pShrMemObj->operationStatus = SHR_MEM_FILLED;
-						  }
-						  sleep(2);
-					 }
-
-					munmap(pShrMemObj, SHARED_MEMORY_OBJECT_SIZE+1);
-					close(shm);
-					shm_unlink(SHARED_MEMORY_OBJECT_NAME);
-
-					break;
-			 }
-
-
+			 processB(file_pipes[0]);	/* reads pipe, create process C*/
 
 			 break;
 		 }
@@ -227,12 +259,8 @@ int main()
 			 /** Process A */
 			 printf("PARENT\n");
 
-			 for(int i = 0; i < 20; i++)
-			 {
-				  data_processed = write(file_pipes[1], some_data, strlen(some_data));
-				  printf("Wrote %d bytes\n", data_processed);
-				  sleep(1);
-			 }
+			 processA(file_pipes[1]);	/* Reads stdI and writes pipe */
+
 			 break;
 		 }
 	 }
